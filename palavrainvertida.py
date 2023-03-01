1fa36190d78baca69fe15002984192f89aceb3e0
Jhosephe Thierry <84161472+jhosephethierry@users.noreply.github.com>
2023-03-01 15:03:14 -0300

    novos

--- palavrainvertida.py
+++ palavrainvertida.py
@@ -9,3 +9,9 @@ for i in range(len(palavra)):
     palavrainvertida = palavrainvertida + palavra[len(palavra)-1-i]
     
     print(palavrainvertida)
+    
+invertida = list(reversed(palavra)) # retorna palavra invertida
+
+print(invertida) # imprime como lista
+
+print(*invertida) # imprime como lista sem virgulas
---
+++ posicoesimpares.py
@@ -0,0 +1,7 @@
+palavra = input("digite uma palavra: ")
+
+for i in range(len(palavra)):
+    
+    if i % 2 == 0: # retorna impares
+        
+        print(f"{i+1} {palavra[i]}")

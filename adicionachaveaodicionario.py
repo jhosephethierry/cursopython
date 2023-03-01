f0b0165923b2534b0e3c3105c32dedd4a7f7d887
Jhosephe Thierry <84161472+jhosephethierry@users.noreply.github.com>
2023-03-01 16:06:42 -0300

    Update adicionachaveaodicionario.py

--- adicionachaveaodicionario.py
+++ adicionachaveaodicionario.py
@@ -1,11 +1,11 @@
 DicionarioA = {
     
-    1: 70,
-    2: 80,
-    3: 90,
+    1: 10,
+    2: 20,
+    3: 30,
    
     }
 
-DicionarioA.update({4: 100})
+DicionarioA.update({4: 40})
 
 print(DicionarioA)

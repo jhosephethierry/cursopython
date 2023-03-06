21da0ccc5182d3897b9fc9d155ac511d57ae553f
Jhosephe Thierry <84161472+jhosephethierry@users.noreply.github.com>
2023-03-06 14:04:27 -0300

    nov

---
+++ listavalores.py
@@ -0,0 +1,6 @@
+def listalimites():
+
+    lista  = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    limiteinferior = int(input("digite o limite inferior: "))
+    limitesuperior = int(input("digite o limite superior: "))
+    intervalo = lista[0:]

f70c4b1a75897a1ffcb831d239648b1fe507974b
Jhosephe Thierry <84161472+jhosephethierry@users.noreply.github.com>
2023-03-06 14:53:14 -0300

    t

--- contaletras.py
+++ contaletras.py
@@ -1,16 +1,18 @@
-def contaletras(palavra,letra):
+def contaletras(palavra,letra): # define a função
     
-    contador = 0
+    contador = 0 # cria uma variável contador iniciando em zero
     
-    for l in palavra:
-        if l.lower() == letra.lower():
-            contador += 1
+    for l in palavra: # condição para
+        
+        if l.lower() == letra.lower(): # .lower() transforma a letra em minúscula pra haver distinção
             
-    return contador
+            contador += 1 # incrementa o contador
+            
+    return contador # retorna o contador
 
-pal = input("digite o texto: ")
-let = input("digite uma letra do texto digitada: ")
+pal = input("digite a(s) palavra(s): ")
+let = input("digite uma letra da(s) palavra(s) digitada(s): ")
 
 contagem = contaletras(pal,let)
 
-print(f"a letra {let} aparece {contagem} vezes na palavra {pal}")
+print(f"a(s) letra(s) {let} aparece(m) {contagem} vez(es) na(s) palavra(s) {pal}")

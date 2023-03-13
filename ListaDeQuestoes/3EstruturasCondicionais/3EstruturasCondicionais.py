5331e55d7b4003d2d8a2275eb246040621ab8a8b
Jhosephe Thierry <84161472+jhosephethierry@users.noreply.github.com>
2023-03-13 14:58:57 -0300

    novo

--- ListaDeQuestoes/3EstruturasCondicionais/3EstruturasCondicionais.py
+++ ListaDeQuestoes/3EstruturasCondicionais/3EstruturasCondicionais.py
@@ -2,3 +2,21 @@
 # a mensagem "Aprovado" se a nota for maior ou igual a 7, "Reprovado" 
 # se a nota for menor que 5 e "Recuperação" se a nota estiver entre 5 e 7.
 
+nomeAluno = input("Digite o nome do aluno: ")
+
+nota1 = float(input("Digite a nota 1: "))
+nota2 = float(input("Digite a nota 2: "))
+nota3 = float(input("Digite a nota 3: "))
+
+somaNotas = nota1 + nota2 + nota3
+
+media = somaNotas/3
+
+print(f"A média do aluno é {media}")
+
+if media >= 7:
+    print(f"{nomeAluno} foi aprovado.")
+elif media <= 5:
+    print(f"{nomeAluno} foi reprovado.")
+elif media > 5 and media < 7:
+    print(f"{nomeAluno} está em recuperação.")

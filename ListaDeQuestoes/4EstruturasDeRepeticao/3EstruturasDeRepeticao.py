8df1d9097dc0e67e63fc8488099a9f190e3f157b
Jhosephe Thierry <84161472+jhosephethierry@users.noreply.github.com>
2023-03-13 16:16:23 -0300

    novos

--- ListaDeQuestoes/4EstruturasDeRepeticao/3EstruturasDeRepeticao.py
+++ ListaDeQuestoes/4EstruturasDeRepeticao/3EstruturasDeRepeticao.py
@@ -1,1 +1,9 @@
 # Escreva um programa que solicite ao usuário um número e imprima a tabuada desse número até o valor 10.
+
+numeroinformado = int(input('Digite um número. '))
+
+for i in range(1,11):
+       
+        calculo = i * numeroinformado
+       
+        print(f"{i}. {numeroinformado} x {i} = {calculo}")

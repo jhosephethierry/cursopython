53c3fa17c99592f48c13c97be956804f6365f638
Jhosephe Thierry <84161472+jhosephethierry@users.noreply.github.com>
2023-03-15 13:21:35 -0300

    novos

--- ListaDeQuestoes/5Funcoes/3Funcoes.py
+++ ListaDeQuestoes/5Funcoes/3Funcoes.py
@@ -1,1 +1,14 @@
 # Escreva uma função que receba uma lista de números inteiros e retorne a soma dos números pares da lista.
+def main():
+
+    lista = []
+
+    for i in range(7):
+
+        listaNumeros = int(input('Digite um número. '))
+        
+        lista.append(listaNumeros)
+
+    print(lista)
+
+main()

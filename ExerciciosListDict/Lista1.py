e6a078c9dd3ad26f0dc592183456d45dae41da1b
Jhosephe Thierry <84161472+jhosephethierry@users.noreply.github.com>
2023-03-30 18:10:01 -0300

    novo

---
+++ ExerciciosListDict/Lista1.py
@@ -0,0 +1,14 @@
+nomes = ["jhosephe","thierry","andrade","cacau"]
+
+while len(nomes) > 1:
+    nomes.pop()
+print(nomes)
+
+nomes.insert(2,"thierry")
+print(nomes)
+
+nomes.insert(3,"andrade")
+print(nomes)
+
+nomes.insert(4,"cacau")
+print(nomes)
---
+++ ExerciciosListDict/Lista2.py
@@ -0,0 +1,6 @@
+nomesA = ["jhosephe","thierry"]
+nomesB = ["andrade","cacau"]
+
+for r in nomesB:
+    nomesA.append(r)
+print(nomesA)

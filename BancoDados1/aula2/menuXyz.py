a927f92992f322ef7c41d603242a73f9efa267ff
Jhosephe Thierry <84161472+jhosephethierry@users.noreply.github.com>
2023-04-12 10:43:44 -0300

    menuxyz

---
+++ BancoDados1/aula2/menuXyz.py
@@ -0,0 +1,30 @@
+import xyz
+
+def menuXYZ():
+
+    opcao = ""
+    while(opcao != '5'):
+        print("")
+        print("Escolha uma opcao no menu.")
+        print("")
+        opcao = input("1. Ver lista de Funcionarios | 2. Ver Lista de Departamentos | 3. Ver Lista por Departamento | 5. Sair: ")
+        
+        if opcao == '1':
+            
+            xyz.ListaFuncionarios()
+          
+        elif opcao == '2':  
+
+            xyz.ListaDepartamentos()
+                             
+        elif opcao == '3':
+
+            xyz.ListaFuncionariosDepartamento()
+
+        elif opcao == '4':
+            print("Voce escolheu sair")
+            break            
+        else:
+            print("Opção Inválida! Tente novamente.")
+
+menuXYZ()

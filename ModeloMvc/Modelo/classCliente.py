f6a5dabab17e24ea2f454738c5ba1e6a88c6c59d
Jhosephe Thierry <84161472+jhosephethierry@users.noreply.github.com>
2023-05-03 13:23:47 -0300

    novo

--- ModeloMvc/Modelo/classCliente.py
+++ ModeloMvc/Modelo/classCliente.py
@@ -1,7 +1,14 @@
 class Cliente:
-
-    def __init__(self, id, nome):
-
+    
+    def __init__(self,id,nome):
         self._id = id
         self._nome = nome
-        
+
+    def sqlInserirCliente(self):
+
+        sql = f'''
+        INSERT INTO "Clientes"
+        Values(default, '{self._nome}')
+        '''
+
+        return sql

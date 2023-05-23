0824bdbb05644b594e2585b92816d9542e046b56
Your Name <you@example.com>
2023-05-23 10:07:37 -0300

    pomar

--- ExerciciosUnidade3/Funcoes.py
+++ ExerciciosUnidade3/Funcoes.py
@@ -25,3 +25,89 @@ def manipularBanco(sql):
     cursor.close()
     conn.close()
 
+
+
+def verPomar():
+
+    pomar = consultarBanco('''
+
+    SELECT * FROM "Pomar"
+    ORDER BY "Id" ASC
+
+    ''')
+
+    if pomar:
+
+        print('''
+        
+    ______________________________________________________________________________
+
+    Pomar da Fazenda 
+    ______________________________________________________________________________
+
+    Estas são as frutas do seu pomar.
+
+    [ Id ]  | Fruta     | Peso      | Estoque       | Data Última Colheita 
+    ______________________________________________________________________________
+
+        ''')   
+
+        for fruta in pomar:
+            print(f" [ {fruta[0]} ] | {fruta[1]}    | {fruta[2]}    | {fruta[3]}    |{fruta[4]}")
+        
+        print("")
+
+        breakpoint
+
+        input("Enter para continuar. ")
+
+    else:
+        
+        print("Seu Pomar está vario.")
+
+        breakpoint
+
+    print("")
+
+
+
+def inserirFruta():
+
+    print("Inserir Frutas")
+
+    fruta = Pomar(None, input("Digite o nome da fruta. "), input("Digite o peso. "), float(input("Digite a quantidade colhida. ")), None)
+
+    manipularBanco(fruta.sqlInserirFruta())
+
+    print("Nova inserida com sucesso!")
+
+
+    op = input("Quer continuar inserindo frutas? s ou n? ")
+    
+    match op:
+
+        case "s":
+            inserirFruta()
+        case "n":
+            print("Saindo!")
+            breakpoint
+        case _:
+            print("Digite uma opção válida.")
+            print("")   
+
+    
+    op = input("Quer visualizar seu Pomar? s ou n? ")
+    
+    match op:
+
+        case "s":
+            verPomar()
+        case "n":
+            print("Saindo!")
+            print("")
+            breakpoint
+        case _:
+            print("Digite uma opção válida.")
+            print("")
+
+    breakpoint

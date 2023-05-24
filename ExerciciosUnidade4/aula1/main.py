9c35203f244e1df2f5f4c7e724795967e1fd8b9b
Jhosephe Thierry <84161472+jhosephethierry@users.noreply.github.com>
2023-05-24 16:21:52 -0300

    ok

--- ExerciciosUnidade4/aula1/main.py
+++ ExerciciosUnidade4/aula1/main.py
@@ -7,24 +7,82 @@
 
 
 
-from flask import Flask
+from flask import *
+import psycopg2
+
+class Conexao:
+
+    def __init__(self, dbname, host, port, user, password):
+        self._dbname = dbname
+        self._host = host
+        self._port = port
+        self._user = user
+        self._password = password
+
+    def consultarBanco(self, sql):
+        try:
+            con = psycopg2.connect(dbname=self._dbname, host=self._host, port = self._port, user = self._user, password = self._password)
+            cursor = con.cursor()
+
+            cursor.execute(sql)
+
+            resultado = cursor.fetchall()
+
+            
+
+            cursor.close()
+            con.close()
+
+
+            return resultado
+        except(Exception, psycopg2.Error) as error:
+            print("Ocorreu um erro no objeto Conexão:", error)
+
+            return False
+        
+    def manipularBanco(self, sql):
+        try:
+            con = psycopg2.connect(dbname=self._dbname, host=self._host, port = self._port, user = self._user, password = self._password)
+            cursor = con.cursor()
+
+            cursor.execute(sql)
+            
+
+            con.commit()
+
+            cursor.close()
+            con.close()
+
+            return True
+        except(Exception, psycopg2.Error) as error:
+            print("Ocorreu um erro:", error)
+
+            return False
+
 
 app = Flask(__name__)
+conexaoBanco = Conexao("pokemon", "localhost", "5432", "postgres", "postgres")
 
 @app.route('/')
 def index():
     
-    return "Hello World"
+    return render_template("index.html")
 
-@app.route('/home')
-def home():
-    
-    return "Goodbye World"
+@app.route('/pokemons')
+def verPokemons():
 
-@app.route('/out')
-def out():
+
+    pokemons = conexaoBanco.consultarBanco('''
+    SELECT * FROM public."Pokedex"
+    ORDER BY "Número Pokedex" ASC  ''')
+
+    if pokemons:
+
+        return render_template("pokemons.html", listaPokemons = pokemons)
     
-    return "Goodbye World"
+    else:
+        return "Ocorreu um erro"
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
